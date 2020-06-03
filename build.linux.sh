ab462ddc0c0a1e35c88284f854af7c228f75127b
Anton Batenev <antonbatenev@yandex.ru>
2020-06-03 18:38:19 +0300

    Fix build.sh for Qt 4/5

--- build.linux.sh
+++ build.linux.sh
@@ -5,19 +5,29 @@ set -e
 # имя проекта
 PROJECT_NAME="avalon"
 
+# версия qt
+if [ -z "${QT_SELECT}" ]; then
+	export QT_SELECT=4
+elif [ "${QT_SELECT}" != "4" ]; then
+	echo "Build require Qt 4.x only :("
+	exit
+fi
+
 # очистка
 if [ -f "Makefile" ]; then
 	make clean
 fi
 
 # создание pro-файла
+BUILD_DIR=$(pwd)
 qmake -project -recursive -Wall -nopwd -o ${PROJECT_NAME}.pro \
-    "CONFIG += debug_and_release" \
-    "QT += network sql webkit" \
-    "LIBS += -laspell -lz" \
-    "macx { QMAKE_CXX = clang }" \
-    "macx { ICON = icons/avalon.icns }" \
-    src
+    "CONFIG      += debug_and_release"                        \
+    "QT          += network sql webkit"                       \
+    "INCLUDEPATH += src"                                      \
+    "LIBS        += -laspell -lz"                             \
+    "macx { QMAKE_CXX = clang }"                              \
+    "macx { ICON = icons/avalon.icns }"                       \
+    "${BUILD_DIR}/src"
 
 # создание make-файлов
 qmake ${PROJECT_NAME}.pro
